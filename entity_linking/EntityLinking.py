adc109964c644408da2f3861f2b2600103c14d06
wenqihe <maggiehwq@gmail.com>
2016-04-23 00:09:46 -0500

    Fix bugs for DBPedia API

--- entity_linking/EntityLinking.py
+++ entity_linking/EntityLinking.py
@@ -6,15 +6,22 @@ import json
 import threading
 import sys, traceback
 import operator
-import math
 
 
 """Entity Linker for a set of documents"""
 #the set where 
-typeDict = []
+typeDict = {}
 docList = {}
-threadNum = 4
-maxDid = 0
+dbpediaToFreebase = {}
+threadNum = 5
+
+def mapping(mapper):
+    f = open(mapper)
+    for line in f:
+        segments = line.split(' ')
+        dbID = segments[0][29:-1]
+        fbID = segments[2][28:-1]
+        dbpediaToFreebase[dbID] = fbID 
 
 class myLinker (threading.Thread):
     docList = {}
@@ -25,69 +32,58 @@ class myLinker (threading.Thread):
         self.docList = docList
         self.offset = offset
         self.confidence = confidence
-        self.docLen = len(docList)
-
     def run(self):
         print "Start DBpediaSpotlight"
         g = open('tmp/temp' + str(self.offset) + '.txt', 'w')
         index = 0
-        blockSize =  math.floor(self.docLen/10.0);
         while 1:
             did = str(index + self.offset)
-            if int(did) <= maxDid:
-                if did in self.docList:
-                    try:
-                        doc = self.docList[did]
-                        url = "http://spotlight.sztaki.hu:2222/rest/annotate"
-                        #url = "http://localhost:2222/rest/annotate"
-                        data = {"confidence":self.confidence}
-                        data["support"] = "20"
-                        data["text"] = doc;
-                        data = urllib.urlencode(data)
-                        req = urllib2.Request(url)
-                        req.add_header('Accept', 'application/json') #text/xml')
-                        print(did),
-                        page = html.fromstring(urllib2.urlopen(req, data, timeout=100).read())
-                        docJson = html.tostring(page)[3:-4]
-                        validEntities = extractAnnotations(docJson)
-                        for entity in validEntities:
-                            types = extractTypes(entity['@types'])
-                            typeStr = ''
-                            for temp in types:
-                                typeStr += temp + ';'
-                            if (entity['@URI'] != None):
-                                if len(types) == 1:
-                                    g.write(str(index + self.offset) + '\t' + entity['@surfaceForm'] + '\t' + typeStr + '\t' + typeStr[:-1] + '\t' + entity['@URI'] + '\t' + entity['@similarityScore'] + '\t' + entity['@percentageOfSecondRank'] + '\n')
-                                elif len(types) > 1:
-                                    g.write(str(index + self.offset) + '\t' + entity['@surfaceForm'] + '\t' + typeStr + '\t' + '' + '\t' + entity['@URI'] + '\t' + entity['@similarityScore'] + '\t' + entity['@percentageOfSecondRank'] + '\n')
-                        index += threadNum
-                    except:
-                        index += threadNum
-                        # print 'noresult'
-                else:
+            if did in self.docList:
+                try:
+                    doc = self.docList[did]
+                    url = "http://spotlight.sztaki.hu:2222/rest/annotate"
+                    #url = "http://localhost:2222/rest/annotate"
+                    data = {"confidence":self.confidence}
+                    data["support"] = "20"
+                    data["text"] = doc;
+                    data = urllib.urlencode(data)
+                    req = urllib2.Request(url)
+                    req.add_header('Accept', 'application/json') #text/xml')
+                    # print did
+                    page = html.fromstring(urllib2.urlopen(req, data, timeout=100).read())
+                    docJson = html.tostring(page)[3:-4]
+                    #print docJson
+                    validEntities = extractAnnotations(docJson)
+                    for entity in validEntities:
+                        linkToFreebase(entity)
+                        if (entity['@URI'] != None):
+                            g.write(str(index + self.offset) + '\t' + entity['@surfaceForm'] + '\t' + entity['@URI'] + '\t'
+                             + entity['@similarityScore'] + '\t' + entity['@percentageOfSecondRank']+ '\n')
                     index += threadNum
-                    continue
+                except:
+                    index += threadNum
+                    print 'noresult'
             else:
                 break
         g.close()
 
 
-def link(docFile, outFile, confidence):
+def link(docFile, confidence):
+    # typeDict['m.01c5'] = None
     # read documents
-    global maxDid
     f = open(docFile)
     for doc in f:
         tab = doc.find('\t')
         did = doc[:tab]
         text = doc[tab+1:]
         docList[did] = text
-        if maxDid < int(did):
-            maxDid = int(did)
     f.close()
 
-    print 'Document #:', len(docList)
-    print 'Max document id:', maxDid
-
+    
+    #for i in range(0, threadNum):
+    #   print 'starrrrr'
+    #   thread.start_new_thread( eachLinker, (i,) )
+    threadLock = threading.Lock()
     threads = []
 
     for i in range(0, threadNum):
@@ -111,7 +107,7 @@ def link(docFile, outFile, confidence):
             allLines[line] = did
 
     sorted_x = sorted(allLines.items(), key=operator.itemgetter(1))
-    g = open(outFile, 'w')
+    g = open('tmp/temp.txt', 'w')
     for tup in sorted_x:
         g.write(tup[0])
     g.close()
@@ -125,37 +121,145 @@ def type(typeFile):
     for line in f:
         myType = line.split('\t')[0]
         if myType != 'NIL':
-            typeDict.append(myType)
+            typeDict[myType] = set()
+            typeDict[myType].add(myType)
     print typeDict
     f.close()
-    return
+    return 
 
-
-# extract dbpedia annotations
+# Extract dbpedia annotations
 def extractAnnotations(docJson):
     validEntities = []
     decoded = json.loads(docJson)
     for entity in decoded['Resources']:
+        # isTarget = False
+        # types = entity['@types'].split(',')
+        # for eType in types:
+        #   if eType in typeSet:
+        #       isTarget = True
+        #       entity['@types'] = eType
+        #       break
         isTarget = True
         if isTarget:
             validEntities.append(entity)
 
     return validEntities
 
+# map dbpedia to freebase
+def linkToFreebase(entity):
+    dbID = entity['@URI'][28:]
+    if dbID in dbpediaToFreebase:
+        entity['@URI'] = dbpediaToFreebase[dbID]
+    else:
+        entity['@URI'] = None
 
 
-# extract types
-def extractTypes(typeStr):
-    types = typeStr.split(',')
-    myTypes = set()
-    for t in types:
-        if t[9:] in typeDict:
-            myTypes.add(t[9:])
-    return myTypes
+# read the freebase entity and extract the notable type
+def findingNotableTypes(freebasekey):
+    f = open('tmp/temp.txt')
+    idToType = {}
+    # Get all the types
+    for line in f:
+        segments = line.strip('\r\n').split('\t')
+        sim_score = float(segments[3])
+        rank_score = float(segments[4])
+        # if sim_score > 0.1 or rank_score == -1: 
+        idToType[segments[2]] = None
+    print 'Find notable type, total# entities: ', len(idToType)
+    # Get notable type for entities, if don't have notable type, remove it
+    index = 0
+    for key in idToType:
+        index += 1
+        if (index % 10000 == 0):
+            print index
+        try:
+            service_url = 'https://www.googleapis.com/freebase/v1/topic'
+            topic_id = '/' + key.replace('.', '/')
+            params = {
+              'key': freebasekey,
+              'filter': '/common/topic/notable_types'
+            }
+            url = service_url + topic_id + '?' + urllib.urlencode(params)
+            topic = json.loads(urllib2.urlopen(url, timeout=10).read())
+            idToType[key] = topic['property']['/common/topic/notable_types']['values'][0]['id']
+        except:
+            continue
+
+    g = open('tmp/temp_2.txt', 'w')
+    f.close()
+    f = open('tmp/temp.txt')
+    #print typeDict
+    # write down the entites
+    for line in f:
+        segments = line.strip('\r\n').split('\t')
+        g.write(segments[0] + '\t' + segments[1] + '\t' + str(idToType[segments[2]]) + '\t' + segments[2]
+         + '\t' + segments[3] + '\t' + segments[4] + '\n')
+    g.close()
+
+
+
+def filterTypes(typeFile, outFile, freebasekey):
+    print 'Filter not-of-interested entities'
+    f = open('tmp/temp_2.txt')
+    g = open(outFile, 'w')
+    typeF = open(typeFile)
+    for line in typeF:
+        myType = line.strip('\r\n').split('\t')[0]
+        if myType != 'NIL':
+            typeDict[myType] = set()
+            typeDict[myType].add(myType)
+    index = 0
+    for line in f:
+        index += 1
+        if index % 10000 == 0:
+            print index
+        segments = line.strip('\r\n').split('\t')
+        myType = segments[2]
+        filteredTypes = exploreType(myType, freebasekey)
+        filteredTypesStr = ''
+        for temp in filteredTypes:
+            filteredTypesStr += temp + ';'
+        g.write(segments[0] + '\t' + segments[1] + '\t' + segments[2] + '\t' + filteredTypesStr + '\t' + segments[3]
+         + '\t' + segments[4] + '\t' + segments[5] + '\n')
+    g.close()
+        
+
+
+# explore the ancestors of a given type
+def exploreType(my_type, freebasekey):
+    if my_type == '/common/topic':
+        return None
+    #print my_type
+    if my_type not in typeDict:
+        try:
+            typeDict[my_type] = set()
+            service_url = 'https://www.googleapis.com/freebase/v1/topic'
+            topic_id = my_type
+            params = {
+              'key': freebasekey,
+              'filter': '/freebase/type_hints/included_types'
+            }
+            url = service_url + topic_id + '?' + urllib.urlencode(params)
+            topic = json.loads(urllib2.urlopen(url, timeout=10).read())
+            for father in topic['property']['/freebase/type_hints/included_types']['values']:
+                fatherID = father['id']
+                filtered = exploreType(fatherID)
+                if (filtered != None):
+                    for temp in filtered:
+                        typeDict[my_type].add(temp)
+        except:
+            return typeDict[my_type]
+
+    return typeDict[my_type]
+
 
 if __name__ == "__main__":
     inFileName='../'+sys.argv[1] # RawText
     typeFile='../'+sys.argv[2]
-    outFile='../'+sys.argv[3]
-    type(typeFile)
-    link(inFileName, outFile, 0.6) # DBpediaSpotlight; Confidence score = 0.6
+    mapFile='../'+sys.argv[3] # ../data/freebase_links.nt
+    outFile='../'+sys.argv[4]
+    freebasekey=sys.argv[5]
+    mapping(mapFile)
+    link(inFileName, 0.2) # DBpediaSpotlight
+    findingNotableTypes(freebasekey) # find notable type for each entity
+    filterTypes(typeFile, outFile, freebasekey) # filter types
--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 DataPath='data/yelp'
-RawText='data/yelp/yelp_sample50k.txt'
+RawText='data/yelp/yelp_sample10.txt'
 TypeFile='data/yelp/type_tid.txt'
 StopwordFile='data/stopwords.txt'
+FreebaseMap='data/freebase_links.nt'
+FreebaseKey='AIzaSyBvkZaBXc1GzVs3d0QN2HjTjDZwlgxboW4' # set your own freebase key
 SegmentOutFile='result/segment.txt'
-SeedFile='result/seed.txt'
+SeedFile='result/seed_yelp.txt'
 significance="1"
 capitalize="1"
 maxLength='4' # maximal phrase length
@@ -26,22 +28,21 @@ pos_path="Intermediate/pos.txt"
 full_pos_path="Intermediate/full_pos.txt"
 frequent_patterns_path="Intermediate/frequentPatterns.pickle"
 segmentInput='Intermediate/phrase_segments.txt'
-cd candidate_generation
-mkdir Intermediate
-python DataPreprocessing/Reformat.py $RawText 
-python DataPreprocessing/Clean.py $RawText 
-python FrequentPhraseMining/FrequentPatternMining.py $segmentInput $maxLength $minSup 
-python EntityExtraction/EntityRelation.py $sentences_path $full_sentence_path $pos_path $full_pos_path $frequent_patterns_path $significance $SegmentOutFile $capitalize
-rm -rf Intermediate
-cd ..
+# cd candidate_generation
+# rm -rf Intermediate
+# mkdir Intermediate
+# python DataPreprocessing/Clean.py $RawText 
+# python FrequentPhraseMining/FrequentPatternMining.py $segmentInput $maxLength $minSup 
+# python EntityExtraction/EntityRelation.py $sentences_path $full_sentence_path $pos_path $full_pos_path $frequent_patterns_path $significance $SegmentOutFile $capitalize
+# cd ..
 
 ### Entity Linking (DBpeidaSpotlight)
 cd entity_linking
 mkdir tmp
-python EntityLinking.py $RawText $TypeFile $SeedFile
-rm -rf tmp
-cd ..
+python EntityLinking.py $RawText $TypeFile $FreebaseMap $SeedFile $FreebaseKey
+#rm -rf tmp
+#cd ..
 
 ### ClusType
-python src/step0-graph_construction.py $SegmentOutFile $StopwordFile $DataStatsFile
-python src/step1-entity_recognition.py $SegmentOutFile $SeedFile $TypeFile $NumRelationPhraseClusters $ResultFile $ResultFileInText
+# python src/step0-graph_construction.py $SegmentOutFile $StopwordFile $DataStatsFile
+# python src/step1-entity_recognition.py $SegmentOutFile $SeedFile $TypeFile $NumRelationPhraseClusters $ResultFile $ResultFileInText
